02a1ebd33adb7b013927cb9d5bc1ccbdb4182af1
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-04-23 05:22:16 +0530

    👩‍🏫teacher-model🛢

---
+++ model/Staff/Teacher.js
@@ -0,0 +1,106 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const TeacherSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    dateEmployed: {
+      type: Date,
+      default: Date.now,
+    },
+    teacherId: {
+      type: String,
+      required: true,
+      default: function () {
+        return (
+          "TEA" +
+          Math.floor(100 + Math.random() * 900) +
+          Date.now().toString().slice(2, 4) +
+          this.name
+            .split(" ")
+            .map((name) => name[0])
+            .join("")
+            .toUpperCase()
+        );
+      },
+    },
+    // * if withdrawn, the teacher will not be able to login.
+    isWithdrawn: {
+      type: Boolean,
+      default: false,
+    },
+    // * if suspended, the teacher can login but cannot perform any task.
+    isSuspended: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      default: "teacher",
+    },
+    subject: {
+      type: Schema.Types.ObjectId,
+      ref: "Subject",
+      required: true,
+    },
+    applicationStatus: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+
+    program: {
+      type: Schema.Types.ObjectId,
+      ref: "Program",
+      required: true,
+    },
+
+    // * A teacher can teach on more than one class level
+    classLevel: {
+      type: Schema.Types.ObjectId,
+      ref: "ClassLevel",
+      required: true,
+    },
+    academicYear: {
+      type: Schema.Types.ObjectId,
+      ref: "AcademicYear",
+      required: true,
+    },
+    examsCreated: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Exam",
+      },
+    ],
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "Admin",
+      required: true,
+    },
+    academicTerm: {
+      type: Schema.Types.ObjectId,
+      ref: "AcademicTerm",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// ! model
+const Teacher = mongoose.model("Teacher", TeacherSchema);
+
+module.exports = Teacher;
