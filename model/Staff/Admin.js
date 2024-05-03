8e2bba31c5d9cae89afa6d16e17fb218b4a61c7d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-05-03 18:45:18 +0530

    💾save-hashed-password🔐-into-database🛢️

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 
 const AdminSchema = new mongoose.Schema(
@@ -24,7 +25,22 @@ const AdminSchema = new mongoose.Schema(
   }
 );
 
+// TODO: hash password
+AdminSchema.pre("save", async function (next) {
+  // * If user is not modifying the password, then simply move to the next middleware
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // TODO: 01. create salt
+  const salt = await bcrypt.genSalt(10);
+  // TODO: 02. Hash password
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
 // ! model
-const Admin = mongoose.model("Admin", adminSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
 
 module.exports = Admin;
