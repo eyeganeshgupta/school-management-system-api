16a4cb09fc109cdd8066345ae037cbea5ba48d5b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-05-10 19:18:19 +0530

    is-loggedIn-middleware-with-token🎟️

--- middlewares/isLogin.js
+++ middlewares/isLogin.js
@@ -1,9 +1,24 @@
-const isLogin = (request, response, next) => {
-  const isLogin = request.userAuth;
-  if (isLogin) {
+const Admin = require("../model/Staff/Admin");
+const verifyToken = require("../utils/verifyToken");
+
+const isLogin = async (request, response, next) => {
+  // TODO: get token from header
+  const headerObj = request.headers;
+  const token = headerObj.authorization.split(" ")[1];
+
+  // TODO: verify token
+  const verifiedToken = verifyToken(token);
+  if (verifiedToken) {
+    // ! find the admin
+    const user = await Admin.findById(verifiedToken.id).select(
+      "name email role"
+    );
+
+    // TODO: save the user into request object
+    request.userAuth = user;
     next();
   } else {
-    const error = new Error("You haven't logged in yet.");
+    const error = new Error("Token expired / invalid");
     next(error);
   }
 };
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -3,9 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (token) => {
   return jwt.verify(token, "ey3", (error, decoded) => {
     if (error) {
-      return {
-        msg: "Invalid token",
-      };
+      return false;
     } else {
       return decoded;
     }
