b42f2608cf022860e303c42e6cccb2930ddad03e
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-05-17 17:36:04 +0530

    express-routes-chaining⛓️

--- routes/academics/academicYearRouter.js
+++ routes/academics/academicYearRouter.js
@@ -11,10 +11,26 @@ const isAdmin = require("../../middlewares/isAdmin");
 
 const academicYearRouter = express.Router();
 
+/*
 academicYearRouter.post("/", isLogin, isAdmin, createAcademicYearCtrl);
 academicYearRouter.get("/", isLogin, isAdmin, getAcademicYearsCtrl);
+*/
+
+academicYearRouter
+  .route("/")
+  .post(isLogin, isAdmin, createAcademicYearCtrl)
+  .get(isLogin, isAdmin, getAcademicYearsCtrl);
+
+/*
 academicYearRouter.get("/:id", isLogin, isAdmin, getAcademicYearCtrl);
 academicYearRouter.put("/:id", isLogin, isAdmin, updateAcademicYearCtrl);
 academicYearRouter.delete("/:id", isLogin, isAdmin, deleteAcademicYearCtrl);
+*/
+
+academicYearRouter
+  .route("/:id")
+  .get(isLogin, isAdmin, getAcademicYearCtrl)
+  .put(isLogin, isAdmin, updateAcademicYearCtrl)
+  .delete(isLogin, isAdmin, deleteAcademicYearCtrl);
 
 module.exports = academicYearRouter;
